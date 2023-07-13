a3c202a1d83d3b2cb3799a42b52054332d1c2c6e
breadworshipper <azhrayashna@gmail.com>
2023-07-13 12:37:56 +0700

    Refactored authentication logic

--- src/api/controllers/authentication.controllers.ts
+++ src/api/controllers/authentication.controllers.ts
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 import { logger } from "../middlewares/winston.logger.js";
 
 async function registerUser(req, res){
-    console.log("Masuk service.")
     const { email, password } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -18,37 +17,33 @@ async function registerUser(req, res){
 
     logger.info(`Email : ${email} has been registered`);
 
-    return newUser;
+    return res.json(newUser);
 }
 
 async function loginUser(req, res){
-    const {email, password} = req.body;
+  const {email, password} = req.body;
 
-    try {
-        const user = await userModel.findOne({ username: email });
-    
-        if (!user) {
-          return res.status(401).send("User not found");
-        }
-    
-        const passwordMatch = await bcrypt.compare(password, user.password);
-    
-        if (passwordMatch) {
-          req.login(user, function (err) {
-            if (err) {
-              console.log(err);
-              return res.status(500).send("Login failed");
-            }
-            console.log("Login success");
-            res.json(user);
-          });
-        } else {
-          return res.status(401).send("Invalid password");
-        }
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send("Login failed");
+  const user = await userModel.findOne({username: email});
+
+  if (!user){
+    return res.status(401).send("User does not exist.");
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (passwordMatch){
+    req.login(user, (err) => {
+      if (err){
+        return res.status(500).send("Login failed.");
       }
+
+      logger.info(`${user.username} just logged in.`);
+    });
+
+    return res.json(user);
+  }
+
+  return res.status(401).send("Invalid password.");
 }
 
 export {registerUser, loginUser};
