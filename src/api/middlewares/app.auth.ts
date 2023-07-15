e0d81d48bc75397920de53f8a96a6ca2ffeab567
breadworshipper <azhrayashna@gmail.com>
2023-07-15 21:29:58 +0700

    Added store session in DB logic

--- src/api/middlewares/app.auth.ts
+++ src/api/middlewares/app.auth.ts
@@ -1,6 +1,10 @@
 import passport from "passport";
 import session from "express-session";
 import {app} from "../../index.js";
+import connectMongo from "connect-mongo";
+import bodyParser from 'body-parser';
+import cookieParser from "cookie-parser";
+import { Strategy as LocalStrategy } from 'passport-local';
 
 import {userModel} from "../models/user.model.js";
 
@@ -8,12 +12,18 @@ async function passportSetup(){
     app.use(session({
         secret: process.env.SECRET,
         resave: false,
-        saveUninitialized: false  
-      }));
+        saveUninitialized: true,
+        store: new connectMongo({mongoUrl: process.env.DEVELOPMENT_DB_URL}),
+        cookie: {
+          maxAge: 1000 * 60 * 60 * 24 * 30 // 30 Days before expiry
+        }  
+      }));   
     app.use(passport.initialize());
     app.use(passport.session());
+
+    const strategy = new LocalStrategy(userModel.authenticate());
     
-    passport.use(userModel.createStrategy());
+    passport.use(strategy);
     passport.serializeUser(userModel.serializeUser());
     passport.deserializeUser(userModel.deserializeUser());
 
