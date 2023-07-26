90a77de18ab89d7d46a46f4b11aea845e9b4ee3c
breadworshipper <azhrayashna@gmail.com>
2023-07-26 12:35:01 +0700

    Further modify budget model

--- src/api/models/budget.model.ts
+++ src/api/models/budget.model.ts
@@ -2,15 +2,28 @@ import mongoose from "mongoose";
 
 const budgetSchema = new mongoose.Schema(
     {
-        ownerId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        ownerId: {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User",
+            require: true
+        },
         name: String,
-        currentSpending: Number,
-        target: Number,
+        currentSpending: {
+            type: Number,
+            default: 0
+        },
+        target: {
+            type: Number,
+            require: true
+        },
         startDate: {
             type: Date,
             default: Date.now()
         },
-        endDate: Date,
+        endDate: {
+            type: Date,
+            require: true
+        },
     }
 );
 
