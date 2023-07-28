7556f6f8b6907d9f02f4ab9bbf5e1e76e0f3945d
breadworshipper <azhrayashna@gmail.com>
2023-07-28 23:21:43 +0700

    Fixed ERR_HTTP_HEADERS_SENT error if token is missing

--- src/api/controllers/budget.controllers.ts
+++ src/api/controllers/budget.controllers.ts
@@ -4,45 +4,43 @@ import { validateToken } from "../middlewares/validate.token.handler.js"
 
 
 async function addBudget(req, res){
-    validateToken(req, res, () => {
+    validateToken(req, res, async () => {
+        // TODO : startDate, endDate
+        const {ownerId, name} = req.body;
+
+        if (!ownerId || !name){
+            res.send("ownerId and name fields are required.");
+        }
+
+        const newBudget = await budgetModel.create({
+            ownerId: ownerId, 
+            name: name
+        });
+
+        logger.info(`${ownerId} has added a ${name} budget.`);
+
+        return res.json({newBudgetId: newBudget.id});
     });
-
-    // TODO : startDate, endDate
-    const {ownerId, name} = req.body;
-
-    if (!ownerId || !name){
-        res.send("ownerId and name fields are required.");
-    }
-
-    const newBudget = await budgetModel.create({
-        ownerId: ownerId, 
-        name: name
-    });
-
-    logger.info(`${ownerId} has added a ${name} budget.`);
-
-    return res.json({newBudgetId: newBudget.id});
 }
 
 async function readBudget(req, res){
-    validateToken(req, res, () => {
+    validateToken(req, res, async () => {
+        const budgetId = req.params.id;
+
+        const budget = await budgetModel.findById(budgetId);
+    
+        console.log(budget);
+    
+        if (!budget){
+            return res.status(400).send(`Budget with id ${budgetId} does not exist.`);
+        }
+    
+        return res.status(200).send(budget);
     });
-
-    const budgetId = req.params.id;
-
-    const budget = await budgetModel.findById(budgetId);
-
-    console.log(budget);
-
-    if (!budget){
-        return res.status(400).send(`Budget with id ${budgetId} does not exist.`);
-    }
-
-    return res.status(200).send(budget);
 }
 
 async function readBudgetByUserId(req, res){
-    validateToken(req, res, () => {
+    validateToken(req, res, async () => {
     });
     
 }
