151fcd1c398bd57a378ad8bfdf1a3394764d1699
breadworshipper <azhrayashna@gmail.com>
2023-07-28 13:13:09 +0700

    Fixed no response if token is missing

--- src/api/middlewares/validate.token.handler.ts
+++ src/api/middlewares/validate.token.handler.ts
@@ -5,12 +5,12 @@ async function validateToken(req, res, next){
 
     const authHeader = req.headers.Authorization || req.headers.authorization;
 
-    if (authHeader && authHeader.startsWith("Bearer")) {
-        token = authHeader.split(" ")[1];
+    if (authHeader == null){
+        return res.status(401).json("User is not authorized or token is missing.");
+    }
 
-        if (!token){
-            return res.status(401).send("User is not authorized or token is missing.");
-        }
+    else if (authHeader && authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1];        
 
         jwt.verify(token, process.env.SECRET, (err, decoded) => {
             if (err){
