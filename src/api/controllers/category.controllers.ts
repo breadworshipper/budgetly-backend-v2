af674d52678c010aaae1171288a7742ee6906080
Yosua Chrial Martono <yosua.chrial@ui.ac.id>
2023-08-06 17:04:47 +0700

    Update category done, change delete category query criteria from category name to category id

--- src/api/controllers/category.controllers.ts
+++ src/api/controllers/category.controllers.ts
@@ -1,6 +1,7 @@
 import { logger } from "../middlewares/winston.logger.js"
 import { categoryModel } from "../models/category.model.js";
 import { validateToken } from "../middlewares/validate.token.handler.js";
+import { log } from "console";
 
 async function categoryExistCheck(
     categoryName: String,
@@ -20,13 +21,11 @@ async function categoryExistCheck(
 }
 
 async function createCategory(req, res) {
-    // validate request
     if (!req.body) {
         res.status(400).send({ message: "Content can not be emtpy!" });
         return;
     }
 
-    // Get prerequisite data
     const categoryName = req.body.categoryName;
 
    await validateToken(req, res, () => { });
@@ -37,7 +36,6 @@ async function createCategory(req, res) {
 
     const categoryExistBool = await categoryExistCheck(categoryName, ownerId);
 
-    // Check category existence
     if (categoryExistBool) {
         res.status(500).send({ message: "Category already existed" })
     }
@@ -78,53 +76,68 @@ async function updateCategory(req, res) {
             .status(400)
             .send({ message: "Data to update can not be empty" })
     }
+
     const id = req.params.id;
+    const { newCategoryName } = req.body;
 
-    const { name } = req.body;
+    await validateToken(req, res, () => { });
+        if(!req.user){ 
+            return;
+        }
+        const ownerId = req.user.id;
 
-    await validateToken(req, res, ()=>{});
-    const ownerId = req.user.id;
+    categoryModel.findById(id).then(data => {
+        if (!data) {
+            res.status(404).send({
+                message: `Category with id ${id} not found`
+            })
+        } else {
+            if (data.owner.toString() !== ownerId){
+                res.status(401).send({
+                    message: `Updating user is not the same as category's owner`
+                })
+            }
+        }
+    })
 
-    const categoryData = {
-        name: name,
+    const newCategoryData = {
+        name: newCategoryName,
         owner : ownerId
     };
 
-    categoryModel.findByIdAndUpdate(id, categoryData, { useFindAndModify: false })
+    categoryModel.findByIdAndUpdate(id, newCategoryData, { useFindAndModify: false })
         .then(data => {
-            if (!data) {
-                res.status(404).send({ message: `Cannot Update category with ID ${id}. Maybe category not found!` })
-            } else {
+            if(data)
                 categoryModel.findById(id).then(data => {
                     logger.info(`Updated category with ID ${id}`)
                     res.send(data)
                 })
             }
-        })
+        )
         .catch(err => {
-            res.status(500).send({ message: "Error Update category information " + err })
+            res.status(500).send({ message: "Error updating category information " + err })
         })
 }
 
 async function deleteCategory(req,res) {
-    const categoryName = req.params.categoryName;
-        categoryModel.findOneAndDelete(categoryName)
+    const id = req.params.id;
+
+        categoryModel.findByIdAndDelete(id)
             .then(data => {
                 if (!data) {
-                    logger.info(`Tried to delete non-existent ${categoryName}`)
-                    res.status(404).send({ message: `Cannot delete ${categoryName}. Maybe name is wrong` })
+                    res.status(404).send({ message: `Category with id ${id} not found` })
                 } else {
                     res.send({
                         message: "Category was deleted successfully!"
                     })
-                    logger.info(`Deleted Category with name ${categoryName}`)
+                    logger.info(`Deleted category with ID ${id}`)
                 }
             })
             .catch(err => {
                 res.status(500).send({
-                    message: `Could not delete ${categoryName}.`
+                    message: `Could not delete category with id= ${id}`
                 });
             });
-}
+    }
 
 export { createCategory, readCategory, updateCategory, deleteCategory }
