726d3386300de786e22e7da6988a6d8c296f47ea
breadworshipper <azhrayashna@gmail.com>
2023-08-07 11:09:22 +0700

    Updated categorySchema#2

--- src/api/models/category.model.ts
+++ src/api/models/category.model.ts
@@ -7,7 +7,7 @@ const categorySchema = new mongoose.Schema(
     }
 );
 
-categorySchema.index({ owner: 1, name: 1 }, { unique: true });
+categorySchema.index({ ownerId: 1, name: 1 }, { unique: true });
 
 const categoryModel = mongoose.model("Category", categorySchema);
 
