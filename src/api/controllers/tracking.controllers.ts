7a7f134afed7c02bac0625b5f2571401959fb916
breadworshipper <azhrayashna@gmail.com>
2023-08-08 11:05:07 +0700

    Prettify tracking.controller.ts

--- src/api/controllers/tracking.controllers.ts
+++ src/api/controllers/tracking.controllers.ts
@@ -1,138 +1,138 @@
-    import { userModel } from "../models/user.model.js"
-    import { logger } from "../middlewares/winston.logger.js"
-    import { trackingModel } from "../models/tracking.model.js";
-    import { categoryModel } from "../models/category.model.js";
-    import { validateToken } from "../middlewares/validate.token.handler.js";
+import { userModel } from "../models/user.model.js"
+import { logger } from "../middlewares/winston.logger.js"
+import { trackingModel } from "../models/tracking.model.js";
+import { categoryModel } from "../models/category.model.js";
+import { validateToken } from "../middlewares/validate.token.handler.js";
 import { createCategory } from "./category.controllers.js";
 
 
-    async function addTracking(req, res) {
-        validateToken(req, res, async () => { 
-            const { name, isExpense, date, categoryName, amount, ownerId } = req.body;
+async function addTracking(req, res) {
+    validateToken(req, res, async () => { 
+        const { name, isExpense, date, categoryName, amount, ownerId } = req.body;
 
-            // validate request
-            if (!name || !isExpense || !date || !categoryName || !amount || !ownerId) {
-                return res.status(400).send({ message: "All fields must be specifed!" });
-            }
+        // validate request
+        if (!name || !isExpense || !date || !categoryName || !amount || !ownerId) {
+            return res.status(400).send({ message: "All fields must be specifed!" });
+        }
 
-            const categoryQueryCriteria = {
-                name: categoryName,
-                ownerId: ownerId
-            }
-            let category;
-            
-            category = await categoryModel.findOne(categoryQueryCriteria);
+        const categoryQueryCriteria = {
+            name: categoryName,
+            ownerId: ownerId
+        }
+        let category;
+        
+        category = await categoryModel.findOne(categoryQueryCriteria);
 
-            console.log(category);
+        console.log(category);
 
-            if (category === null){
-                // create new category
-            }
+        if (category === null){
+            // create new category
+        }
 
-        logger.info(category);
-        const tracking = await trackingModel.create({
-            name: name,
-            isExpense: isExpense,
-            date: date,
-            category: category,
-            amount: amount,
-            owner : ownerId
-        });
-        logger.info(`A new tracking has been created`)
+    logger.info(category);
+    const tracking = await trackingModel.create({
+        name: name,
+        isExpense: isExpense,
+        date: date,
+        category: category,
+        amount: amount,
+        owner : ownerId
+    });
+    logger.info(`A new tracking has been created`)
 
-            if (tracking) {
-                return res.status(201).json({ _id: tracking.id })
-            }
-        });
+        if (tracking) {
+            return res.status(201).json({ _id: tracking.id })
+        }
+    });
+}
+
+async function readTracking(req, res) {
+    if (req.body.id) {
+        const id = req.body.id;
+
+        trackingModel.findById(id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({ message: "Not found tracking with id " + id })
+                } else {
+                    logger.info(`Sent tracking data with ID ${id}`)
+                    res.send(data)
+                }
+            })
+            .catch(err => {
+                res.status(500).send({ message: "Error retrieving tracking with id " + id })
+            })
+
+    } else {
+        trackingModel.find()
+            .then(data => {
+                logger.info(`Sent all tracking data`)
+                res.send(data)
+            })
+            .catch(err => {
+                res.status(500).send({ message: err.message || "Error Occurred while retriving tracking information" })
+            })
     }
+}
 
-    async function readTracking(req, res) {
-        if (req.body.id) {
-            const id = req.body.id;
+async function updateTracking(req, res) {
+    if (!req.body) {
+        return res
+            .status(400)
+            .send({ message: "Data to update can not be empty" })
+    }
+    const id = req.params.id;
 
-            trackingModel.findById(id)
-                .then(data => {
-                    if (!data) {
-                        res.status(404).send({ message: "Not found tracking with id " + id })
-                    } else {
-                        logger.info(`Sent tracking data with ID ${id}`)
-                        res.send(data)
-                    }
-                })
-                .catch(err => {
-                    res.status(500).send({ message: "Error retrieving tracking with id " + id })
-                })
+    const { name, isExpense, date, category, amount } = req.body;
 
-        } else {
-            trackingModel.find()
-                .then(data => {
-                    logger.info(`Sent all tracking data`)
+    await validateToken(req, res, () => { });
+    const ownerId = req.user.id;
+
+    const trackingData = {
+        name: name,
+        isExpense: isExpense,
+        date: date,
+        category: await categoryModel.findOne({ category, ownerId }),
+        amount: amount
+    };
+
+    trackingModel.findByIdAndUpdate(id, trackingData, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot Update tracking with ID ${id}. Maybe tracking not found!` })
+            } else {
+                trackingModel.findById(id).then(data => {
+                    logger.info(`Updated tracking with ID ${id}`)
                     res.send(data)
                 })
-                .catch(err => {
-                    res.status(500).send({ message: err.message || "Error Occurred while retriving tracking information" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Error Update tracking information " + err })
+        })
+}
+
+async function deleteTracking(req, res) {
+    const id = req.params.id;
+
+    trackingModel.findByIdAndDelete(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Tracking with id ${id} not found` })
+            } else {
+                res.send({
+                    message: "Tracking was deleted successfully!"
                 })
-        }
-    }
-
-    async function updateTracking(req, res) {
-        if (!req.body) {
-            return res
-                .status(400)
-                .send({ message: "Data to update can not be empty" })
-        }
-        const id = req.params.id;
-
-        const { name, isExpense, date, category, amount } = req.body;
-
-        await validateToken(req, res, () => { });
-        const ownerId = req.user.id;
-
-        const trackingData = {
-            name: name,
-            isExpense: isExpense,
-            date: date,
-            category: await categoryModel.findOne({ category, ownerId }),
-            amount: amount
-        };
-
-        trackingModel.findByIdAndUpdate(id, trackingData, { useFindAndModify: false })
-            .then(data => {
-                if (!data) {
-                    res.status(404).send({ message: `Cannot Update tracking with ID ${id}. Maybe tracking not found!` })
-                } else {
-                    trackingModel.findById(id).then(data => {
-                        logger.info(`Updated tracking with ID ${id}`)
-                        res.send(data)
-                    })
-                }
-            })
-            .catch(err => {
-                res.status(500).send({ message: "Error Update tracking information " + err })
-            })
-    }
-
-    async function deleteTracking(req, res) {
-        const id = req.params.id;
-
-        trackingModel.findByIdAndDelete(id)
-            .then(data => {
-                if (!data) {
-                    res.status(404).send({ message: `Tracking with id ${id} not found` })
-                } else {
-                    res.send({
-                        message: "Tracking was deleted successfully!"
-                    })
-                    logger.info(`Deleted tracking with ID ${id}`)
-                }
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message: `Could not delete Tracking with id= ${id}`
-                });
+                logger.info(`Deleted tracking with ID ${id}`)
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Could not delete Tracking with id= ${id}`
             });
-    }
+        });
+}
 
-    // ... Klo ada yang kurang tambahkan
+// ... Klo ada yang kurang tambahkan
 
-    export { addTracking, readTracking, updateTracking, deleteTracking }
+export { addTracking, readTracking, updateTracking, deleteTracking }
