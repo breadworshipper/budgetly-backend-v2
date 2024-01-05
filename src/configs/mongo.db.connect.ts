6fc69889f03a118a28d68ec906d9726d7c2f6faa
breadworshipper <azhrayashna@gmail.com>
2024-01-05 23:23:18 +0700

    Minor db logging change

--- src/configs/mongo.db.connect.ts
+++ src/configs/mongo.db.connect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB(dbUrl: string){
     mongoose.connect(dbUrl);
-    console.log("Connection established");
+    console.log(`Connection established ${dbUrl}`);
 }
 
 export {connectDB}
