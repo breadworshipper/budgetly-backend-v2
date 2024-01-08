990a8aeab88d53f1f29ebc65c537eaa53004f7e6
breadworshipper <azhrayashna@gmail.com>
2024-01-08 16:36:47 +0700

    Fix: Get stats for a specific user

--- src/api/controllers/stats.controller.ts
+++ src/api/controllers/stats.controller.ts
@@ -2,28 +2,61 @@ import { validateToken } from "../middlewares/validate.token.handler.js";
 import { trackingModel } from "../models/tracking.model.js";
 import { logger } from "../middlewares/winston.logger.js";
 import { categoryModel } from "../models/category.model.js";
+import { ObjectId } from "mongodb";
 
 async function countTracking(req, res) {
-  
-        validateToken(req, res, async () => {
-            const nonNullTrackingCount = await trackingModel.countDocuments({ category: { $ne: null } });
-    const categories = await categoryModel.find()
-    const trackingCountPromises = categories.map(async (categoryObject) => {
-        const categoryTotal = await trackingModel.countDocuments({ category: categoryObject.id });
-        const categoryInfo = {
-          categoryName: categoryObject.name,
-          categoryTotal: categoryTotal,
-          categoryPercentage: (categoryTotal / nonNullTrackingCount) * 100,
-        };
-        return categoryInfo;
-      });
-  
-      const trackingCountList = await Promise.all(trackingCountPromises);
-      
-      logger.info(trackingCountList);
-  
-      res.send(trackingCountList);
-        });
+  validateToken(req, res, async () => {
+    const ownerId = req.params.id; // Assuming the userId is passed as a parameter
+
+    console.log(`userId: ${ownerId}`)
+
+    // Get the total amount of a user tracking
+    const nonNullTrackingAmountTotal = await trackingModel.aggregate([
+      {
+        $match: {
+          ownerId: new ObjectId(ownerId),
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalAmount: {
+            $sum: "$amount",
+          },
+        },
+      }
+    ]);
+
+    // Stringify the result
+    const nonNullTrackingAmountTotalString = JSON.stringify(nonNullTrackingAmountTotal);
+    console.log(`nonNullTrackingAmountTotalString: ${nonNullTrackingAmountTotalString}`)
+    
+    // Get each category name and its total amount
+    const categoryTotalAmount = await trackingModel.aggregate([
+      {
+        $match: {
+          ownerId: new ObjectId(ownerId),
+        },
+      },
+      {
+        $group: {
+          _id: "$category",
+          totalAmount: {
+            $sum: "$amount",
+          },
+        },
+      },
+    ]);
+
+    // For each category, get the category name
+    for (let i = 0; i < categoryTotalAmount.length; i++) {
+      const categoryName = await categoryModel.findById(categoryTotalAmount[i]._id);
+      categoryTotalAmount[i].percentage = categoryTotalAmount[i].totalAmount / nonNullTrackingAmountTotal[0].totalAmount * 100;
+      categoryTotalAmount[i].categoryName = categoryName.name;
+    }
+
+    res.json(categoryTotalAmount)
+  });
 }
 
 export {countTracking}
